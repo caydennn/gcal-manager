54afbe387127661e794b564e913bdacf118bc19e
caydennn <gweeyongta@gmail.com>
2021-09-06 12:19:22 +0800

    Updated comments

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def getService():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
